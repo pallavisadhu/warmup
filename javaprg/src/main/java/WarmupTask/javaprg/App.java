ca787c9162af59fece8c61d6f2018652d0dc04b3
pallavisadhu <pallavisadhu@yahoo.com>
2022-12-05 15:59:35 -0600

    warm up task

---
+++ javaprg/src/main/java/WarmupTask/javaprg/App.java
@@ -0,0 +1,13 @@
+package WarmupTask.javaprg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ javaprg/src/test/java/WarmupTask/javaprg/FindTwo2.java
@@ -0,0 +1,39 @@
+package WarmupTask.javaprg;
+
+import java.util.ArrayList;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FindTwo2 {
+	
+	public ArrayList<Integer> two2(ArrayList<Integer> al) {
+		
+		ArrayList<Integer> output = new ArrayList<Integer>();
+		int res=0;
+		for(int n : al) {
+			res = n*2;
+			if(res%10!=2) {
+				output.add(res);
+				
+			}
+		}
+		System.out.println(output);
+		return output;
+	}
+	
+	
+	@Test
+	public void testCase1() {
+		ArrayList<Integer> al =new ArrayList<Integer>();
+		al.add(2);
+		al.add(6);
+		al.add(11);
+		ArrayList<Integer> expected = two2(al);
+		ArrayList<Integer> actual = new ArrayList<Integer>();
+		actual.add(4);
+		//actual.add(6);
+		Assert.assertEquals(actual, expected);
+	}
+
+}
