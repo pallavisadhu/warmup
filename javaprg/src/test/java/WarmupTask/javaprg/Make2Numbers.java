31e83027957e17f0d3bf1ff6dd4cc41885c70323
pallavisadhu <pallavisadhu@yahoo.com>
2022-12-07 15:52:15 -0600

    added another class

---
+++ javaprg/src/test/java/WarmupTask/javaprg/Make2Numbers.java
@@ -0,0 +1,67 @@
+package WarmupTask.javaprg;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Make2Numbers {
+	
+	public int[] make2(int[] a,int[] b) {
+		
+		int []result = new int[2];
+		
+		int len = a.length;
+		
+		if(len>=2) {
+			for(int j=0;j<2;j++)
+			result[j] = a[j];
+		}else if(len>=1 && len<2) {
+			for(int j=0;j<1;j++)
+			{
+				result[0] = a[0];
+				result[1]=b[j];
+			}
+		}else {
+			for(int j=0;j<2;j++) {
+				result[j]=b[j];
+			}
+		}
+		for(int i=0;i<2;i++) {
+			System.out.println(result[i]);
+		}
+	return result;		
+	}
+	
+	
+	
+	
+	@Test
+	public void testCase1() {
+		int[] a= {};
+		int[] b= {1,2};
+		
+		int[] output = make2(a,b);
+		int[] expected = {1,2};
+		Assert.assertEquals(output, expected);
+	}
+	
+	@Test
+	public void testCase2() {
+		int[] a= {4,5};
+		int[] b= {1,2,3};
+		
+		int[] output = make2(a,b);
+		int[] expected = {4,5};
+		Assert.assertEquals(output, expected);
+	}
+	
+	@Test
+	public void testCase3() {
+		int[] a= {4};
+		int[] b= {1,2,3};
+		
+		int[] output = make2(a,b);
+		int[] expected = {4,1};
+		Assert.assertEquals(output, expected);
+	}
+
+}
